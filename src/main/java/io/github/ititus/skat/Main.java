fd01ea7c98928c225062623f8a3be021234089b9
iTitus <iTitus@users.noreply.github.com>
2020-12-26 01:34:23 +0100

    add netty

--- src/main/java/io/github/ititus/skat/Main.java
+++ src/main/java/io/github/ititus/skat/Main.java
@@ -1,30 +1,33 @@
 package io.github.ititus.skat;
 
+import io.github.ititus.skat.network.NetworkClient;
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    static {
+        System.setProperty("java.util.logging.config.file", "logging.properties");
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        VBox vb = new VBox();
-        vb.setSpacing(10);
-        vb.setAlignment(Pos.CENTER);
+    public void start(Stage stage) throws Exception {
+        Text t = new Text(10, 20, "Hello World");
 
-        Text t = new Text("Hello World");
-        vb.getChildren().add(t);
+        Group root = new Group(t);
 
-        Scene scene = new Scene(vb, 640, 480);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Skat");
-        primaryStage.show();
+        Scene scene = new Scene(root, 640, 480);
+        stage.setScene(scene);
+        stage.setTitle("Skat");
+        stage.show();
+
+        NetworkClient client = new NetworkClient();
     }
 }
